6508c71b62e887c63ad5fc4051db946cd3891350
vivek <vkjangid056@gmail.com>
2023-11-21 22:37:42 +0530

    3

---
+++ p3.py
@@ -0,0 +1,12 @@
+def prime(n):
+    if n == 0 or n== 1:
+        return "number is prime"
+    else:
+        for i in range(2, n):
+            if n%i ==0:
+                return "number is not prime"
+        else:
+            return "number is prime"
+        
+
+print(prime(11))
